726c8779c77323fb0f87e12026b545bc10d7fa83
Polina <vecherdobrii007@gmail.com>
2022-09-18 02:07:08 +0300

    added grpc commands to buttons

--- ClientWPF/MVVM/ViewModel/MainWindowViewModel.cs
+++ ClientWPF/MVVM/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,49 @@
-﻿using ClientWPF.Core;
+﻿using System;
+using System.Numerics;
+using System.Windows.Input;
+using ClientWPF.Core;
+using ClientWPF.MVVM.Commands;
+using GrpcClient;
 
 namespace ClientWPF.MVVM.ViewModel;
 
 public class MainWindowViewModel : ObservableObject
 {
+    private string _answer;
+    private string _userName;
+    public ICommand ConnectToTheServerCommand { get; set; }
+
     public object CurrentView { get; set; }
 
-    public MainWindowViewModel()
+    public string UserName
     {
-        CurrentView = new AdminPanelViewModel();
+        get => _userName;
+        set
+        {
+            _userName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Answer
+    {
+        get => _answer;
+        set
+        {
+            _answer = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainWindowViewModel(object currentView)
+    {
+        CurrentView = currentView;
+        ConnectToTheServerCommand = new RelayCommand(ConnectToTheServer);
+    }
+
+    private async void ConnectToTheServer(object o)
+    {
+        var a = await new ChatClientFunctions().Login(UserName);
+        Answer = a.Code == 0 ? "You are now connected!" : a.Message;
     }
 }

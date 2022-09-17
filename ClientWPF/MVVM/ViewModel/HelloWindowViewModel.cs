dfa370e2ece24b4389e8a6d1a701894097547cec
Polina <vecherdobrii007@gmail.com>
2022-09-18 02:06:00 +0300

    added welcome window view model

---
+++ ClientWPF/MVVM/ViewModel/HelloWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using ClientWPF.Core;
+using ClientWPF.MVVM.Commands;
+using DryIoc;
+
+namespace ClientWPF.MVVM.ViewModel;
+
+public class HelloWindowViewModel : ObservableObject
+{
+    public ICommand OpenAdminWindowCommand { get; set; }
+    public ICommand OpenUserWindowCommand { get; set; }
+    private readonly Window _owner;
+
+    public HelloWindowViewModel(Window owner)
+    {
+        _owner = owner;
+
+        OpenAdminWindowCommand = new RelayCommand(o =>
+        {
+            var a = App.Container.Resolve<MainWindow>();
+            a.DataContext = new MainWindowViewModel(new AdminPanelViewModel());
+            a.Show();
+            _owner.Close();
+        });
+        OpenUserWindowCommand = new RelayCommand(o =>
+        {
+            var a = App.Container.Resolve<MainWindow>();
+            a.DataContext = new MainWindowViewModel(new UserPanelViewModel());
+            a.Show();
+            _owner.Close();
+        });
+    }
+}

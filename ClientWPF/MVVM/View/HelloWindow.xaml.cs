95b41ab48459a79a474c6a44bd287fc54ba48bd8
Polina <vecherdobrii007@gmail.com>
2022-09-18 02:05:17 +0300

    added welcome window

---
+++ ClientWPF/MVVM/View/HelloWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ClientWPF.MVVM.View;
+
+public partial class HelloWindow : Window
+{
+    public HelloWindow()
+    {
+        InitializeComponent();
+    }
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+            DragMove();
+    }
+
+    private void Minimaze_Click(object sender, RoutedEventArgs e)
+    {
+        if (Application.Current.MainWindow != null)
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+    }
+
+    private void Maximaze_Click(object sender, RoutedEventArgs e)
+    {
+        if (Application.Current.MainWindow != null)
+            Application.Current.MainWindow.WindowState =
+                Application.Current.MainWindow.WindowState != WindowState.Maximized
+                    ? WindowState.Maximized
+                    : WindowState.Normal;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}

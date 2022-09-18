b215cb097b98605a5f77c6c8c407427c06bf872f
Polina <vecherdobrii007@gmail.com>
2022-09-19 00:52:39 +0300

    added error traps and chat display

--- ClientWPF/MVVM/ViewModel/AdminPanelViewModel.cs
+++ ClientWPF/MVVM/ViewModel/AdminPanelViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System.Numerics;
+﻿using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using ClientWPF.Core;
 using ClientWPF.MVVM.Commands;
 using DryIoc;
-using GrpcClient;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using LUC;
 using static Cryptography.Extensions.ByteArrayExtensions;
 
@@ -17,6 +20,7 @@ public class AdminPanelViewModel : ObservableObject
     private string _privateLucKey;
     private string _nLucKey;
     private byte[] _dealkeybyte;
+    private MainWindowViewModel _mainwindowVM;
     public ICommand GenerateDEALkeyCommand { get; set; }
     public ICommand GenerateLUCkeysCommand { get; set; }
 
@@ -26,6 +30,7 @@ public class AdminPanelViewModel : ObservableObject
 
     public AdminPanelViewModel()
     {
+        _mainwindowVM = App.Container.Resolve<MainWindowViewModel>();
         GenerateDEALkeyCommand = new RelayCommand(o =>
         {
             _dealkeybyte = GenerateRandomByteArray(16);
@@ -37,18 +42,60 @@ public class AdminPanelViewModel : ObservableObject
 
     private async void SendDEALkey(object o)
     {
-        var mainwindowVM = App.Container.Resolve<MainWindowViewModel>();
-        
-        var encryptedDealKey = new LUC.LUC().Encrypt(new BigInteger(_dealkeybyte), LUCkeys.PublicKey);
-        await mainwindowVM.Client.SendKey(encryptedDealKey.ToByteArray());
+        if (DEALkey == null)
+        {
+            _mainwindowVM.Messages = "[memo] Generate DEAL key to send it\n";
+            return;
+        }
+
+        if (PrivateLUCkey == null || PublicLUCkey == null || NLUCkey == null)
+        {
+            _mainwindowVM.Messages = "[memo] Generate LUC keys to send DEAL key\n";
+            return;
+        }
+
+        if (_mainwindowVM.ConnectionCode == 1)
+        {
+            var encryptedDealKey = new LUC.LUC().Encrypt(new BigInteger(_dealkeybyte), LUCkeys.PublicKey);
+            await _mainwindowVM.Client.SendKey(encryptedDealKey.ToByteArray());
+            _mainwindowVM.Messages = "[memo] Now you can chatting\n";
+            Task.Run(() => Chatting());
+        }
+
+        else _mainwindowVM.Messages = "[memo] Join the server to get started\n";
     }
 
     private void GenerateLUCkeys(object o)
     {
+        if (DEALkey == null)
+        {
+            _mainwindowVM.Messages = "[memo] Generate DEAL key to generate LUC keys\n";
+            return;
+        }
+
         LUCkeys = new KeyGenerator(new BigInteger(Encoding.Default.GetBytes(DEALkey)));
         PublicLUCkey = LUCkeys.PublicKey.Key.ToString();
         PrivateLUCkey = LUCkeys.PrivateKey.Key.ToString();
         NLUCkey = LUCkeys.PrivateKey.N.ToString();
+    }
+
+    private async void Chatting()
+    {
+        var dataStream = _mainwindowVM.Client._streamingClient.ChatMessagesStreaming(new Empty());
+        await foreach (var messageData in dataStream.ResponseStream.ReadAllAsync())
+        {
+            //vm.Messages.Add(new MessageBoxControl($"[{DateTime.Now}]{messageData.User}: {messageData.Message}"));
+            if (_mainwindowVM.Encoder == null)
+            {
+                _mainwindowVM.InitCipher();
+            }
+            /*var tmp = Encoding.UTF8.GetBytes(messageData.Message.ToStringUtf8());
+            var mes = _mainwindowVM.Cipher.Decrypt(tmp, _mainwindowVM.RoundKeys);
+            _mainwindowVM.Messages =
+                $"[{messageData.Time}] {messageData.User} : {Encoding.UTF8.GetString(mes)}\n";*/
+            _mainwindowVM.Messages =
+                $"[{messageData.Time}] {messageData.User} : {messageData.Message.ToStringUtf8()}\n";
+        }
     }
 
     public string DEALkey
--- ClientWPF/MVVM/ViewModel/UserPanelViewModel.cs
+++ ClientWPF/MVVM/ViewModel/UserPanelViewModel.cs
@@ -1,12 +1,12 @@
 ﻿using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Windows;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using ClientWPF.Core;
 using ClientWPF.MVVM.Commands;
 using DryIoc;
-using GrpcClient;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using LUC;
 
 namespace ClientWPF.MVVM.ViewModel;
@@ -17,24 +17,9 @@ public class UserPanelViewModel : ObservableObject
     private string _privateLucKey;
     private string _nLucKey;
     private string _username;
-
     public ICommand GetDEALkeyCommand { get; set; }
-    public UserPanelViewModel()
-    {
-        GetDEALkeyCommand = new RelayCommand(GetDEALkey);
-    }
+    private MainWindowViewModel _mainwindowVM;
 
-    private async void GetDEALkey(object o)
-    {
-        var mainwindowVM = App.Container.Resolve<MainWindowViewModel>();
-        _username = mainwindowVM.UserName;
-        
-        var a = await mainwindowVM.Client.GetKey(_username);
-        var privatekey = new LucKey(BigInteger.Parse(PrivateLUCkey),
-            BigInteger.Parse(NLUCkey));
-        var decrypted = new LUC.LUC().Decrypt(new BigInteger(a.Key.ToArray()), privatekey);
-        DEALkey = decrypted.ToString();
-    }
     public string DEALkey
     {
         get => _dealkey;
@@ -44,6 +29,7 @@ public class UserPanelViewModel : ObservableObject
             OnPropertyChanged();
         }
     }
+
     public string PrivateLUCkey
     {
         get => _privateLucKey;
@@ -61,6 +47,46 @@ public class UserPanelViewModel : ObservableObject
         {
             _nLucKey = value;
             OnPropertyChanged();
+        }
+    }
+
+    public UserPanelViewModel()
+    {
+        _mainwindowVM = App.Container.Resolve<MainWindowViewModel>();
+        GetDEALkeyCommand = new RelayCommand(GetDEALkey);
+    }
+
+    private async void GetDEALkey(object o)
+    {
+        if (_mainwindowVM.ConnectionCode == 1)
+        {
+            if (PrivateLUCkey == null || NLUCkey == null)
+            {
+                _mainwindowVM.Messages = "[memo] Enter LUC keys to get DEAL key\n";
+                return;
+            }
+
+            _username = _mainwindowVM.UserName;
+            var a = await _mainwindowVM.Client.GetKey(_username);
+            var privatekey = new LucKey(BigInteger.Parse(PrivateLUCkey),
+                BigInteger.Parse(NLUCkey));
+            var decrypted = new LUC.LUC().Decrypt(new BigInteger(a.Key.ToArray()), privatekey);
+            DEALkey = decrypted.ToString();
+            _mainwindowVM.Messages = "[memo] Now you can chatting\n";
+            Task.Run(() => Chatting(_mainwindowVM));
+            
+        }
+
+        else _mainwindowVM.Messages = "[memo] Join the server to get started\n";
+    }
+
+    private async void Chatting(MainWindowViewModel vm)
+    {
+        var dataStream = vm.Client._streamingClient.ChatMessagesStreaming(new Empty());
+        await foreach (var messageData in dataStream.ResponseStream.ReadAllAsync())
+        {
+            //vm.Messages.Add(new MessageBoxControl($"[{DateTime.Now}]{messageData.User}: {messageData.Message}"));
+            vm.Messages = $"[{messageData.Time}] {messageData.User} : {messageData.Message.ToStringUtf8()}\n";
         }
     }
 }

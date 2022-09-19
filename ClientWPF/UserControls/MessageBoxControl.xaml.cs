d74495cdb096f266bd0a51e494972685bd9977ce
Polina <vecherdobrii007@gmail.com>
2022-09-19 14:18:22 +0300

    added new control for displaying messages in the chat

---
+++ ClientWPF/UserControls/MessageBoxControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using GrpcClient;
+
+namespace ClientWPF.UserControls;
+
+public partial class MessageBoxControl : UserControl
+{
+    public string Message { get; set; }
+    public string UserName { get; set; }
+    public string Time { get; set; }
+
+    public MessageBoxControl(string username, string time, string message)
+    {
+        InitializeComponent();
+        Message = message;
+        UserName = username;
+        Time = time;
+    }
+    
+}
